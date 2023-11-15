649a4f4adfa49a9f1f41ac77ce28b5591b4948b3
Ida Lindell <ida.lindell@nordnet.se>
2023-11-15 14:24:17 +0100

    Added ssr and deploy to netlify

---
+++ src/pages/api/test.ts
@@ -0,0 +1,16 @@
+import type { APIRoute } from "astro";
+
+export async function GET() {
+  return new Response(JSON.stringify({ msg: "Hello World!" }), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+}
+
+export const POST: APIRoute = async ({ request, params }) => {
+  const body = await request.json();
+  return new Response(JSON.stringify(body), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+};
